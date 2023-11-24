967f2360a9fe3d5ecb4c608377ef008bc9391eb5
BATENDER <vinicius.torres.san@gmail.com>
2023-11-24 15:02:46 -0300

    OK

--- aula03-RepeticoesEArrays/atividades/js/exe02.js
+++ aula03-RepeticoesEArrays/atividades/js/exe02.js
@@ -1,8 +1,6 @@
 // 2 - Fazer a tabuada de 8 com repetições
 
-var resultado;
-
 for (var i = 0; i <= 10; i++) {
     resultado = i * 8;
-    console.log(`${i} x 8 = ${resultado}`);
+    console.log(`${i} x 8 = ${i * 8}`);
 }
--- aula03-RepeticoesEArrays/atividades/js/exe03.js
+++ aula03-RepeticoesEArrays/atividades/js/exe03.js
@@ -1,10 +1,9 @@
 // 3 - Fazer as tabuadas de 1 até 10 com repetições
 
-var resultado, num;
+var num = Number(prompt('Digite a tabuada do número desejado: '));
 
-num = Number(prompt('Digite a tabuada do número desejado: '));
-
-for (var i = 0; i <= 10; i++) {
+// Tabuada do número até deu quadrado perfeito
+for (var i = 0; i <= num; i++) {
     resultado = i * num;
-    console.log(`${i} x ${num} = ${resultado}`);
+    console.log(`${i} x ${num} = ${i * num}`);
 }

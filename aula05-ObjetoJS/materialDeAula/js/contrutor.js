43735309f0f113c548fbcc15a6b53eb63354bf84
BATENDER <vinicius.torres.san@gmail.com>
2023-11-27 17:37:46 -0300

    aula05 - Objeto JS

---
+++ aula05-ObjetoJS/materialDeAula/js/contrutor.js
@@ -0,0 +1,24 @@
+class Produto {
+    constructor(valorId, valorNome, valorMarca, valorPreco, valorQtdEstoque, valorQtdVendida) {
+        this.id = valorId;
+        this.nome = valorNome;
+        this.marca = valorMarca;
+        this.preco = valorPreco;
+        this.qtdEstoque = valorQtdEstoque;
+        this.qtdVendida = valorQtdVendida;
+    }
+    id;
+    nome;
+    marca;
+    preco;
+    qtdEstoque;
+    qtdVendida;
+    venda(qtd) {
+        this.qtdEstoque -= qtd;
+        this.qtdVendida += qtd;
+        return `Foram vendidos ${qtd} do produto ${this.nome}`;
+    }
+}
+
+var produto1 = new Produto(1, 'Feijão', 'Seu Jorge', 15.99, 100, 0);
+var produto2 = new Produto(2, 'Coca-cola', 'Coca', 3, 50, 200);
---
+++ aula05-ObjetoJS/materialDeAula/js/objeto.js
@@ -0,0 +1,39 @@
+class Cliente {
+    nome;
+    cpf;
+    endereco;
+    telefone;
+    conta;
+    saldo;
+
+    // AÇÃO  = FUNÇÃO(MÉTODO)
+    verificaValor() {
+        return `Saldo atual R$${this.saldo}!`
+    }
+    sacar(valor) {
+        // this.atributo = seleciona um atributo modelo
+        if (this.saldo <= valor) {
+            return `Transação NÃO aprovada!`
+        } else {
+            this.saldo = this.saldo - valor;
+            return `Transação de R$${valor} aprovada! ${this.verificaValor()}`;
+        }
+    }
+    depositar(valor) {
+        if (valor > 0) {
+            this.saldo = this.saldo + valor;
+            return `Transação de R$${valor} aprovada! ${this.verificaValor()}`;
+        } else {
+            return `Não se pode depositar valores negativos ou nulos!`
+        }
+    }
+}
+
+var cliente1 = new Cliente();
+
+    cliente1.nome = 'Leonardo';
+    cliente1.cpf = '12345678910';
+    cliente1.endereco = 'sim rua 12 casa do não te interessa'
+    cliente1.telefone = '81912345678'
+    cliente1.conta = '123';
+    cliente1.saldo = 0;

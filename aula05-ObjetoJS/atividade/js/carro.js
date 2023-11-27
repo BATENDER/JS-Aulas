2bd01b48d1735b3b3021ffc9d16cefef63a7c105
BATENDER <vinicius.torres.san@gmail.com>
2023-11-27 17:52:23 -0300

    Atividade 6 - começo

---
+++ aula05-ObjetoJS/atividade/js/carro.js
@@ -0,0 +1,13 @@
+class Carro {
+    constructor(marca, modelo, ano, cor, velocidadeMax, velocidadeAtual) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+        this.velocidadeMax = velocidadeMax;
+        this.velocidadeAtual = velocidadeAtual;
+    }
+    acelerar(num) {
+        
+    }
+}
